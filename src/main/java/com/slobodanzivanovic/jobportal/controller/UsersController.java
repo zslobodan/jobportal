bdbb318e403186652b9d4415d5f549d11b4b02fd
Slobodan Zivanovic <192605563+zslobodan@users.noreply.github.com>
2025-01-10 18:28:09 +0100

    Added login, logout, initial dashboard

--- src/main/java/com/slobodanzivanovic/jobportal/controller/UsersController.java
+++ src/main/java/com/slobodanzivanovic/jobportal/controller/UsersController.java
@@ -4,8 +4,15 @@ import com.slobodanzivanovic.jobportal.entity.Users;
 import com.slobodanzivanovic.jobportal.entity.UsersType;
 import com.slobodanzivanovic.jobportal.services.UsersService;
 import com.slobodanzivanovic.jobportal.services.UsersTypeService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,5 +54,22 @@ public class UsersController {
 
 		usersService.addNew(users);
 		return "dashboard";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+		}
+
+		return "redirect:/";
 	}
 }
--- src/main/java/com/slobodanzivanovic/jobportal/services/UsersService.java
+++ src/main/java/com/slobodanzivanovic/jobportal/services/UsersService.java
@@ -7,6 +7,11 @@ import com.slobodanzivanovic.jobportal.repository.JobSeekerProfileRepository;
 import com.slobodanzivanovic.jobportal.repository.RecruiterProfileRepository;
 import com.slobodanzivanovic.jobportal.repository.UsersRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -40,5 +45,24 @@ public class UsersService {
 
 	public Optional<Users> getUserByEmail(String email) {
 		return usersRepository.findByEmail(email);
+	}
+
+	public Object getCurrentUserProfile() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			String username = authentication.getName();
+			Users users = usersRepository.findByEmail(username)
+					.orElseThrow(() -> new UsernameNotFoundException("Could not found " + username));
+			int userId = users.getUserId();
+			if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))) {
+				return recruiterProfileRepository.findById(userId).orElse(new RecruiterProfile());
+			} else {
+				return jobSeekerProfileRepository.findById(userId).orElse(new JobSeekerProfile());
+			}
+		}
+
+		return null;
 	}
 }
